2302e6e6f1da61edc9e6397133019a33b3758efa
Rahsheen Porter <rahsheen.porter@gmail.com>
2019-11-24 14:58:11 -0500

    Attempt to fix eslint

--- rnExample/.eslintrc.js
+++ rnExample/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {    
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
+  }
 };
