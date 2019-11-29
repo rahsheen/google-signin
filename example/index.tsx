6d1a7e36ce9dcc760dbec85fdfa64068e65bb8c9
Rahsheen Porter <rahsheen.porter@gmail.com>
2019-11-28 23:17:45 -0500

    Add dotenv and samples

--- example/index.tsx
+++ example/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { useGoogleSignIn } from '../.';
 import config from './googleSigninConfig';
+require('dotenv').config();
 
 const App = () => {
   const { userInfo, error, signIn, signOut, loading } = useGoogleSignIn(config);
